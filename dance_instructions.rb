db9252020f5be7776d46a62f3fd64e0b0fb26a59
EmiKnape <enknape@aol.com>
2018-06-04 23:18:25 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,4 +1,7 @@
 
 # Code your methods below
 def dance_instructions
-  puts 
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "lift right foot"
+  puts "Return the right foot to the ground"
+  puts "Finish with a small skip-step backward"
